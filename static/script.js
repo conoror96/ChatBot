eb43cd83ffdba157476392ca85aa42d21e2dd887
kodama96 <g00338592@gmit.ie>
2017-11-23 15:44:46 +0000

    update js with comments

--- static/script.js
+++ static/script.js
@@ -1,34 +1,36 @@
-const conversation = $("#list"); 
+const conversation = $("#list"); // $ means jquery, it won't get the item without the #
 const userInput = $("#userInput");
 console.log(conversation);
 
 
 userInput.keypress(function (event) {
-    if (event.keyCode !== 13) { 
+    if (event.keyCode !== 13) { // User presses enter
         return; 
     }
-    event.preventDefault();
+    event.preventDefault(); // prevents page refresh
     const text = userInput.val(); 
 
     console.log(text);
-    userInput.val(""); 
-    if (text.trim() == "") { 
+    userInput.val(""); // set userInput to nothing
+
+    if (text.trim() == "") { //if statement for trimming spaces
         return;
     }
 
-    queryParameters = {    
+    queryParameters = {     // a query parameter user-input is expected
         "userInput": text
     }
 
- conversation.append("<li id='inputBox' align='right' class=\"list-group\">" + text + "<li class=\"list-group\">");
- $.get("/ask", queryParameters).done(function (resp) {
-       
-        setTimeout(function () { 
+    conversation.append("<li id='inputBox' align='right' class=\"list-group\">" + text + "<li class=\"list-group\">");
+    //http://localhost:8080
+    $.get("/ask", queryParameters).done(function (resp) {
+        // this code will execute when the request gets a response.
+        setTimeout(function () { // wait 1 second then add element.
             conversation.append("<li id='outputBox' align='left' class=\"list-group\">" + resp + "<li class=\"list-group\">");
         }, 1000);
 
-    }).fail(function () {
-        conversation.append("<li class=\"list-group\">the bot Can't talk right now!</li class=\"list-group\">");
+    }).fail(function () { // error / fail message 
+        conversation.append("<li class=\"list-group\">The bot Can't talk right now!</li class=\"list-group\">");
     });
 
     window.scrollTo(0, document.body.scrollHeight);
